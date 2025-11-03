642312d24e529d89b20b87e0d72013cd5d6d3edf
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2025-11-03 21:17:41 +0600

    BEE_1013

---
+++ Beecrowd/BEE_1013.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+"""
+
+a, b, s = map(int, input().split())
+
+greatest = max(a, b, s)
+
+print(f"{greatest} eh o maior")
