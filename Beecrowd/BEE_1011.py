bd3e0a1f9b3291d36bb947d97c4589f9f75de437
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2025-11-03 21:09:05 +0600

    BEE_1011

---
+++ Beecrowd/BEE_1011.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+"""
+
+R = int(input())
+
+radius = (4 / 3) * 3.14159 * R**3
+
+print("VOLUME = {:.3f}".format(radius))
