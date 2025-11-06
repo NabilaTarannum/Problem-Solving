7f315be893108451403052fe8ddb8b46d4a4ab97
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2025-11-06 21:09:36 +0600

    BEE_1017

---
+++ Beecrowd/BEE_1017.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+"""
+
+spent_time = int(input())
+average_speed = int(input())
+
+liters = average_speed * spent_time / 12
+
+print("{:.3f}".format(liters))
